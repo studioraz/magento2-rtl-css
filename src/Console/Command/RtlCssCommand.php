6f79c9f4d4e21c77c1a6f6c034ea4a3539158ba5
Itay Raz <itay@studioraz.co.il>
2023-11-10 14:23:53 +0200

    first commit

---
+++ src/Console/Command/RtlCssCommand.php
@@ -0,0 +1,63 @@
+<?php
+/*
+ * Copyright © 2023 Studio Raz. All rights reserved.
+ * See LICENSE.txt for license details.
+ */
+
+namespace SR\RTLCss\Console\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Process\Process;
+use Symfony\Component\Process\ProcessFactory;
+use Magento\Framework\Module\Dir;
+class RtlCssCommand extends  Command
+
+{
+
+
+    public function __construct(
+        private readonly ProcessFactory $processFactory,
+        private readonly Dir $moduleDir,
+        string           $name = null
+    ) {
+
+        parent::__construct($name);
+    }
+
+    protected function configure()
+    {
+        $this->setName('studioraz:rtl-css:rtl-css')
+            ->setDescription('Generate RTL CSS files.');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        // Get the root directory of SR_RTLCss module
+        $rootDir = $this->moduleDir->getDir('SR_RTLCss');
+
+        /** @var Process $process */
+        $process = $this->processFactory->create([
+            'command' => ['npx', 'rtlcss', 'style.css', 'style.css'],
+            'cwd' => $rootDir
+        ]);
+
+        //$process->setWorkingDirectory($rootDir);
+
+        $process->run();
+
+        // Print the output of the command to the shell
+        $output->writeln($process->getOutput());
+
+
+        if (!$process->isSuccessful()) {
+            $output->writeln($process->getErrorOutput());
+            return 1;  // Non-zero exit code to signify that the command failed
+        }
+
+        $output->writeln('RTL CSS files generated successfully.');
+        return 0;  // Zero exit code to signify that the command was successful
+    }
+
+}
