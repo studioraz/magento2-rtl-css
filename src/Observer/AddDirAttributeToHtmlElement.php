e15113de3d803452a3028b880c9c9dc848aac272
Maksym <maxim@studioraz.co.il>
2026-04-27 13:16:57 +0300

    KIN-434 Added a per-request guard in the observer so the attribute is set only once.

--- src/Observer/AddDirAttributeToHtmlElement.php
+++ src/Observer/AddDirAttributeToHtmlElement.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Copyright © 2025 Studio Raz. All rights reserved.
  * See LICENSE for license details.
@@ -11,30 +10,41 @@ namespace SR\RTLCss\Observer;
 
 use Magento\Framework\Event\Observer;
 use Magento\Framework\Event\ObserverInterface;
+use Magento\Framework\Registry;
 use Magento\Framework\View\Page\Config;
 use SR\RTLCss\Service\LocaleWritingDirectionService;
 
 class AddDirAttributeToHtmlElement implements ObserverInterface
 {
+    private const REGISTRY_FLAG = 'sr_rtlcss_dir_set';
+
     protected Config $pageConfig;
     protected LocaleWritingDirectionService $rtlManager;
+    private Registry $registry;
 
     public function __construct(
         Config $pageConfig,
         LocaleWritingDirectionService $rtlManager,
+        Registry $registry
     ) {
         $this->pageConfig = $pageConfig;
         $this->rtlManager = $rtlManager;
+        $this->registry = $registry;
     }
 
     public function execute(Observer $observer): void
     {
+        if ($this->registry->registry(self::REGISTRY_FLAG)) {
+            return;
+        }
+        $this->registry->register(self::REGISTRY_FLAG, true);
+
         $direction = $this->rtlManager->getStoreViewContentDirection();
 
         $this->pageConfig->setElementAttribute(
             Config::ELEMENT_TYPE_HTML,
             LocaleWritingDirectionService::HTML_ATTRIBUTE_DIR,
-            $direction,
+            $direction
         );
     }
 }
